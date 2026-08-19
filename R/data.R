1fe77ba76e9dab14634ec88f0c03f1a4eead8fd5
Hugo Gruson <git@hugogruson.fr>
2026-08-19 10:35:15 +0200

    Update documentation

--- R/data.R
+++ R/data.R
@@ -1,4 +1,36 @@
-HD2013SGIdata <- function(dataset) {
+#' Download a dataset from the HD2013SGI package
+#'
+#' This function downloads a dataset from the HD2013SGI package and caches it
+#' locally. If the dataset has already been downloaded, it will be loaded from
+#' the cache instead of downloading it again.
+#'
+#' It replaces the previous datasets that were included in the package.
+#'
+#' @param dataset The name of the dataset to download. This should be one of the
+#' following: "datamatrix", "datamatrixfull", "featuresPerWell", "Interactions",
+#' "mainEffects", "nrOfInteractionsPerTarget", "QueryAnnotation",
+#' "stabilitySelection", or "TargetAnnotation".
+#'
+#' @returns The requested dataset
+#'
+#' @examples
+#' \donttest{
+#' HD2013SGIdata("datamatrix")
+#' }
+HD2013SGIdata <- function(
+  dataset = c(
+    "datamatrix",
+    "datamatrixfull",
+    "featuresPerWell",
+    "Interactions",
+    "mainEffects",
+    "nrOfInteractionsPerTarget",
+    "QueryAnnotation",
+    "stabilitySelection",
+    "TargetAnnotation"
+  )
+) {
+  dataset <- match.arg(dataset)
   filename <- paste0(dataset, ".rds")
   resolved <- httr2::request("https://doi.org/10.5281/zenodo.21995286") |>
     httr2::req_method("HEAD") |>
